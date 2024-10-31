2d29ec8b91ecc69d84acbacd5f53dc416561914f
Maximilian Grätz <max@MacBook-Pro-von-Maximilian.local>
2024-10-31 19:28:03 +0100

    switched password

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,7 +11,7 @@ const typeOrmConfig: TypeOrmModuleOptions = {
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: '',
   database: process.env.DB_NAME,
   entities: [Level, Card, Player],
   synchronize: true,
